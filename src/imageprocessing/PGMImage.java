3a21dbfd0b8e92d0168df8eeabba49da26211391
rlebail <rlebail@pc-b13-09.eleves.ecn>
2014-12-01 09:38:58 +0100

    GEOFFRAY MANGE DU SAUCISSON

---
+++ src/imageprocessing/PGMImage.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageprocessing;
+
+/**
+ *
+ * @author rlebail
+ */
+public class PGMImage {
+    
+    private int l;
+    private int h;
+    private final static int greyScale = 255;
+    private final static int lineLength = 70;
+    private int[][] pixelArray;
+    
+}
