2122bc3053e106fd1fd0670fb0f77ab48c5d744a
gmenudie <gmenudie@pc-b13-14.eleves.ecn>
2014-12-01 11:00:11 +0100

    Création du writer pour enregistrer l'image dans un fichier .pgm

--- src/imageprocessing/Writer.java
+++ src/imageprocessing/Writer.java
@@ -4,13 +4,42 @@
  */
 package imageprocessing;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
 
 /**
- *
- * @author rlebail
+ *Gère l'écriture dans fichier
+ * @author gmenudie & oliviernguyen
  */
 public class Writer {
- 
-    
-    
+
+    /**
+     *Crée unn fichier .pgm a partir d'un PGMImage
+     * @param image
+     */
+    public static void writeToFile(PGMImage image) {
+
+        try {
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter("image.pgm"));
+            bw.write("P2 \n#\n");
+            bw.write(image.getL()+" "+image.getH()+"\n");
+            bw.write(Integer.toString(PGMImage.greyScale)+"\n");
+         
+            for(int i=0;i<(image.getL()*image.getH());i++ ){
+                bw.write(Integer.toString(image.getPixelArray()[i])+" ");
+                if((i+1)%image.getL()==0)
+                {
+                    bw.newLine();
+                }
+            }   
+            bw.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
