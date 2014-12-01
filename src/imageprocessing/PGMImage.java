7828a05e53c9b3e8d4c51e2681ec1fc60aba62da
rlebail <rlebail@pc-b13-09.eleves.ecn>
2014-12-01 10:18:53 +0100

    DIFF

--- src/imageprocessing/PGMImage.java
+++ src/imageprocessing/PGMImage.java
@@ -53,14 +53,44 @@ public class PGMImage {
         return pixelArray;
     }
     
+    public void fillPixelArray(int [] a) {
+        if(a.length != pixelArray.length) {
+            System.out.println("Size error");}
+        else {
+            for(int i=0; i<a.length; i++){ 
+                pixelArray[i]=a[i];
+            }       
+        }
+    }
+    
     public void fillImage() {
      
         for(int i=0; i<pixelArray.length; i++){
             
-            pixelArray[i] = Math.round( (float) i/greyScale );
+            pixelArray[i] = Math.round( (float) i*greyScale/pixelArray.length );
         }
         
     }
     
+    public PGMImage diff(PGMImage I){
+       
+    if(!(l==I.getL() && h==I.getH())) {
+        System.out.println("Les images n'ont pas la même taille");
+        return I;
+    }
+        
+    PGMImage delta = new PGMImage(l,h);
+    int[] dataArray = I.getPixelArray();
+    int[] resArray = null;
     
+    for(int i=0; i<pixelArray.length; i++){
+            
+       resArray[i] = pixelArray[i]-dataArray[i]>0?pixelArray[i]-dataArray[i]:0;
+    }
+    
+    delta.fillPixelArray(resArray);
+    
+    return delta;           
+    }
+                
 }
