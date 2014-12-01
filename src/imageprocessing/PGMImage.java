56bc08a5d252b2d32c2e08608f9b6ddd82a6ecb5
rlebail <rlebail@pc-b13-09.eleves.ecn>
2014-12-01 10:00:29 +0100

    GETTERS ET SETTERS

--- src/imageprocessing/PGMImage.java
+++ src/imageprocessing/PGMImage.java
@@ -10,10 +10,57 @@ package imageprocessing;
  */
 public class PGMImage {
     
+    public final static int greyScale = 255;
+    public final static int lineLength = 70;
+    
     private int l;
     private int h;
-    private final static int greyScale = 255;
-    private final static int lineLength = 70;
-    private int[][] pixelArray;
+    private int[] pixelArray;
+
+    public PGMImage(int largeur, int hauteur){
+        l=largeur;
+        h=hauteur;
+    
+    for(int i=0;i<l*h;i++) pixelArray[i]=0;
+        
+    }
+    
+   public PGMImage(PGMImage I){
+        l = I.getL();
+        h = I.getH();
+        pixelArray = I.getPixelArray().clone();
+    } 
+       
+    
+    /**
+     * @return the l
+     */
+    public int getL() {
+        return l;
+    }
+
+    /**
+     * @return the h
+     */
+    public int getH() {
+        return h;
+    }
+
+    /**
+     * @return the pixelArray
+     */
+    public int[] getPixelArray() {
+        return pixelArray;
+    }
+    
+    public void fillImage() {
+     
+        for(int i=0; i<pixelArray.length; i++){
+            
+            pixelArray[i] = Math.round( (float) i/greyScale );
+        }
+        
+    }
+    
     
 }
